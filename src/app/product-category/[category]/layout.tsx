f485498608b4bfd2a7a27b13085e171dcd96a9d0
ahnajafi4252 <ahnajafi4252@gmail.com>
2025-12-01 08:53:05 +0330

    fixed responsive for sorting category box

--- src/app/product-category/[category]/layout.tsx
+++ src/app/product-category/[category]/layout.tsx
@@ -4,6 +4,7 @@ import SideFilter from '@/components/SideFilter'
 import SortingFilter from '@/components/SortingFilter'
 import { Icon } from '@iconify/react'
 import { use, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 type Props = {
     children: React.ReactNode
@@ -12,34 +13,57 @@ type Props = {
 
 export default function CtegoryProductLayout({ children, params }: Props) {
 
+    const [showSide, setShowSide] = useState(false)
+    const [showSort, setShowSort] = useState(false)
+
     const { category } = use(params)
 
-    const [showSide, setShowSide] = useState(false)
+    const searchParams = useSearchParams()
+    const currentSort = searchParams.get('sort') || "default"
+
+    const sortLabels: Record<string, string> = {
+        default: "همه کالاها",
+        highPrice: "بیشترین قیمت",
+        lowPrice: "کمترین قیمت",
+        discount: "بیشترین تخفیف"
+    }
+    const categoryLabels: Record<string, string> = {
+        mobile: "موبایل",
+        laptop: "لپ تاپ",
+        watch: "ساعت هوشمند",
+        tablet: "تبلت",
+        airpod: "هندزفری و ایرپاد",
+        game: "کنسول بازی"
+    }
 
     return (
         <div className="sm:mx-10 my-5">
             <div className="flex flex-col gap-10">
                 <div className="w-full flex">
                     {/* side filter */}
-                    <SideFilter category={category} showSide={showSide} onClose={() => setShowSide(false)}/>
+                    <SideFilter category={category} showSide={showSide} onClose={() => setShowSide(false)} />
 
 
                     {/* product table */}
                     <div className="w-full sm:w-4/5 flex flex-col gap-2 mr-0 sm:mr-5 mt-2">
                         {/* breadcrumb */}
-                        <div className="mr-2 my-2">فراز شاپ / موبایل</div>
+                        <div className="mr-2 my-2 flex items-center gap-1">
+                            <span className='text-gray-500'>فراز شاپ</span>
+                            <Icon className='text-gray-500' icon="solar:alt-arrow-left-outline" />
+                            {categoryLabels[category]}
+                        </div>
 
                         {/* product filter  */}
-                        {/* <SortingFilter category={params.category}/> */}
+                        <SortingFilter category={category} showSort={showSort} onClose={() => setShowSort(false)} />
 
-                        <div className='grid grid-cols-2 divide-x-[1px] lg:hidden border border-gray-300 rounded-xl px-2'>
+                        <div className='grid grid-cols-2 divide-x divide-gray-200 lg:hidden border border-gray-300 rounded-xl mx-2'>
                             <button className="flex items-center justify-center gap-2 py-3" onClick={() => setShowSide(true)}>
                                 <Icon className="text-xl" icon="solar:filter-outline" />
-                                <span>فیلترها</span>
+                                <span className='text-sm'>فیلترها</span>
                             </button>
-                            <button className="flex items-center justify-center gap-2 py-3">
+                            <button className="flex items-center justify-center gap-2 py-3" onClick={() => setShowSort(true)}>
                                 <Icon className="text-2xl" icon="solar:sort-from-top-to-bottom-outline" />
-                                <span>ترتیب: همه کالاها</span>
+                                <span className='text-sm'>ترتیب: {sortLabels[currentSort!]}</span>
                             </button>
                         </div>
                         {children}
--- src/components/SortingFilter.tsx
+++ src/components/SortingFilter.tsx
@@ -6,39 +6,43 @@ import { useSearchParams } from "next/navigation";
 
 type Props = {
     category: string
+    showSort: boolean
+    onClose: () => void
 }
 
-export default function SortingFilter({ category }: Props) {
+export default function SortingFilter({ category, showSort, onClose }: Props) {
 
     const searchParams = useSearchParams();
     const currentSort = searchParams.get("sort") || "default";
 
     const options = [
-        { label: "همه‌ محصولات", value: "default" },
+        { label: "همه‌ کالاها", value: "default" },
         { label: "بیشترین قیمت", value: "highPrice" },
         { label: "کمترین قیمت", value: "lowPrice" },
         { label: "بیشترین تخفیف", value: "discount" }
     ]
-    
+
 
     return (
-        <div className="flex items-center justify-between border border-gray-300 rounded-2xl p-4">
-            <div className="flex items-center gap-4">
-                <div className="flex items-center gap-2 text-gray-900 font-medium">
-                    <Icon className="text-2xl" icon="solar:sort-from-top-to-bottom-outline" />
-                    <span>مرتب سازی:</span>
+        <div className={`${showSort ? 'bg-gray-500/50 backdrop-blur-xs w-full h-screen fixed z-50 top-0' : 'hidden lg:block'}`} onClick={onClose}>
+            <div className={`${showSort ? 'relative top-1/3 right-1/4 w-fit px-10 py-4' : 'p-4'} bg-white flex items-center justify-between border border-gray-300 rounded-2xl`}>
+                <div className={`${showSort ? 'flex-col' : ''} flex items-center gap-4`}>
+                    <div className="flex items-center gap-2 text-gray-900 font-medium">
+                        <Icon className="text-2xl" icon="solar:sort-from-top-to-bottom-outline" />
+                        <span>مرتب سازی:</span>
+                    </div>
+                    <ul className={`${showSort ? 'flex-col' : ''} flex items-center gap-8 text-gray-700 text-sm`}>
+                        {options.map(option => (
+                            <li key={option.value} onClick={onClose}>
+                                <Link href={`/product-category/${category}?sort=${option.value}`} className={currentSort === option.value ? 'text-sky-500' : ''}>
+                                    {option.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-                <ul className="flex items-center gap-8 text-gray-700 text-sm">
-                    {options.map(option => (
-                        <li key={option.value}>
-                            <Link href={`/product-category/${category}?sort=${option.value}`} className={currentSort === option.value ? 'text-sky-500' : ''}>
-                                {option.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                {/* <span>3,500 کالا</span> */}
             </div>
-            <span>3,500 کالا</span>
         </div>
     )
 }
